6a1fc768d5a2dfde689f72dee2663f8ab4d4bf84
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-17 15:18:22 -0700

    Add files via upload

---
+++ ClientApp/src/components/Levels.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+
+const Levels = () => (
+    <div>
+        <h1>Levels</h1>
+        <p>Collection of connected rooms</p>
+    </div>
+    );
+
+export default connect()(Levels);
---
+++ ClientApp/src/components/Single.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+
+const Single = () => (
+    <div>
+        <h1>Single Room</h1>
+        <p>Page for displaying single room.</p>
+    </div>
+    );
+
+export default connect()(Single);
