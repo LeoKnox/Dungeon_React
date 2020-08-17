721dd2b6ed83eaed814a6855c6b8cf7fbc2df997
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-17 15:09:59 -0700

    Add files via upload

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
+import Rooms from './components/Rooms';
+import Single from './components/Single';
+import Levels from './components/Levels';
 
 import './custom.css'
 
---
+++ ClientApp/src/components/Rooms.tsx
@@ -0,0 +1,48 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+
+const allRooms = [
+    {
+        name: "Entry",
+        floor: "Wood",
+        wall: "Stone",
+        width: 5,
+        height: 5
+    }
+]
+
+const AllRooms = () => ( // original code props instead of  ()
+    <div>
+        <h1>All Rooms</h1>
+        {renderRooms()}
+    </div>
+);
+
+function renderRooms() {
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Floor</th>
+                    <th>Wall</th>
+                    <th>Width</th>
+                    <th>Height</th>
+                </tr>
+            </thead>
+            <tbody>
+                {allRooms.map(room =>
+                    <tr key={room.name}>
+                        <td>{room.name}</td>
+                        <td>{room.floor}</td>
+                        <td>{room.wall}</td>
+                        <td>{room.width}</td>
+                        <td>{room.height}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    );
+}
+
+export default connect()(AllRooms);
