202b82fa579748afd21ff4c3efade49c7dbe4f6c
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-19 19:33:06 -0700

    Add files via upload

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -3,13 +3,25 @@ import { connect } from 'react-redux';
 import { useState } from 'react';
 
 function Build() {
+    const [character, setCharacter] = useState("Eveehi");
     const [status, setStatus] = useState("Open");
+    const [level, setLevel] = useState(1);
     return (
+        <>
+        <div>
+            <h3>Character playing {character}</h3>
+                <button onClick={() => setCharacter("Aelien")}>New Character</button>
+        </div>
+            <div>
+                <p>Character Level {level}</p>
+                <button onClick={() => setLevel(level+1)}>New Level</button>
+            </div>
         <div>
             <h3>Build: {status} </h3>
             <button onClick={() => setStatus("Closed")}>Close</button>
             <button onClick={() => setStatus("Open")}>Open</button>
         </div>
+        </>
          )
    }
 
