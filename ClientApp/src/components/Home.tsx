465683153addbec59f950d707eac87d516055dda
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-19 17:58:37 -0700

    Add files via upload

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -1,31 +1,27 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-const warriorList = [
-    { id: 1, name: "Aelien", class: "Fighter" },
-    { id: 2, name: "Eveehi", class: "Wizard" },
-    { id: 3, name: "Xingu", class: "Thief" }
-];
-
-function Players() {
+function Build() {
     return (
         <div>
-            {warriorList.map((warrior, i) =>
-                <div key={warrior.id}>
-                    <h2>{i}:{warrior.name}</h2>
-                    <p>Class: {warrior.class}</p>
-                </div>
-            )}
-            </div>
-         );
+            <h3>Build dungeons</h3>
+        </div>
+         )
    }
+
+function Play() {
+    return (
+        <div>
+            <h3>Play dungeons</h3>
+        </div>
+        )
+}
    
    const Home = (props: any) => (
-  <div>
-    <h1>Welcome to dungeon builder</h1>
-        <p>Star building and making dungeons here.</p>
-           {Players()}
-    </div>
+       <React.Fragment>
+           <Play />
+           <Build />
+       </React.Fragment>
 );
 
 export default connect()(Home);
