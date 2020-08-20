0fd0d47c7f7c4ee2e77dc6bf4f26ccb2aa062e30
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-19 18:35:21 -0700

    Add files via upload

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
+const [first, second, third] = [
+    "Aelien",
+    "Eveehi",
+    "Xingu"
+]
+
 function Build() {
     return (
         <div>
             <h3>Build dungeons</h3>
+            <p>{first}</p>
+            <p>{third}</p>
+            <p>{second}</p>
         </div>
          )
    }
