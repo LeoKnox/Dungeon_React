4b76b5557cdfcba46041c405422f1cbf513314f0
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-20 19:15:17 -0700

    Add files via upload

--- ClientApp/src/components/Home.jsx
+++ ClientApp/src/components/Home.jsx
@@ -2,14 +2,23 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { useState, useEffect } from 'react';
 
-funct GH(login) {
+function GH(login) {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        fetch(`https://ap.github.com/users/${login}`)
+            .then(res => res.json())
+            .then(setData)
+            .catch(console.error);
+    }, []);
 
+    if (data) {
+        return <div>{JSON.stringify(data)}</div>;
+    }
+    return null;
 }
 
 function Build() {
-    return (
-            return <GH login="moonhighway"/>
-        )
+    return <GH login="moonhighway" />;
    }
 
    const Home = () => (
